d9346de87539bdae859f96bae03a476560227a9b
Md Zaki Hussain <mdzakihusain@gmail.com>
2023-01-02 11:41:06 +0530

    Added files manually

---
+++ index.js
@@ -0,0 +1,16 @@
+let count=0
+let countEl= document.getElementById("count-el")
+let saveEl = document.getElementById("save-el")
+function increment(){
+    count += 1
+    countEl.textContent= count
+}
+
+function save(){
+    //work on it
+    saveEl.innerText= saveEl.innerText + " " +count + " - " 
+    count = 0
+    countEl.textContent=0
+}
+
+
